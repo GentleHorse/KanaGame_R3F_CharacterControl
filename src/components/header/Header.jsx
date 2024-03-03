d889218193b2b768890574a3eeaa545d8f0f5226
GentleHorse <anch.abc.t@gmail.com>
2024-03-03 12:39:33 +0100

    Added google fonts to tailwind

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,29 @@
 export default function Header() {
-    return (
-      <header className="fixed mt-5 ml-5 z-10">
-        <section>
-          <p className="font-roboto mt-[10px] text-xs text-amber-300">This is header text.</p>
-        </section>
-      </header>
-    );
-  }
+  return (
+    <header className="fixed mt-5 ml-5 z-10">
+      <section>
+        <p className="font-roboto mt-[10px] text-[20px] text-amber-300">
+          This is "robot" font.
+        </p>
+        <p className="font-micro-5 mt-[10px] text-[20px] text-amber-300">
+          This is "micro-5" font.
+        </p>
+        <p className="font-permanent-marker mt-[10px] text-[20px] text-amber-300">
+          This is "permanent-marker" font.
+        </p>
+        <p className="font-shadows-into-light mt-[10px] text-[20px] text-amber-300">
+          This is "shadows-into-light" font.
+        </p>
+        <p className="font-new-tegomin mt-[10px] text-[20px] text-amber-300">
+          此れは、「ニューテゴミン」の書体である。
+        </p>
+        <p className="font-yuji-syuku mt-[10px] text-[20px] text-amber-300">
+          此れは、「佑字 肅」の書体である。
+        </p>
+        <p className="font-dot-gothic-16 mt-[10px] text-[20px] text-amber-300">
+          此れは、「ドットゴシック１６」の書体である。
+        </p>
+      </section>
+    </header>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,12 @@ export default {
     extend: {
       fontFamily: {
         'roboto': ['Roboto', 'sans-serif'],
+        'micro-5': ['"Micro 5", sans-serif'],
+        'permanent-marker': ['"Permanent Marker", cursive'],
+        'shadows-into-light': ['"Shadows Into Light", cursive'],
+        'new-tegomin': ['"New Tegomin", serif'],
+        'yuji-syuku': ['"Yuji Syuku", serif'],
+        'dot-gothic-16': ['"DotGothic16", sans-serif'],
       }
     },
   },
